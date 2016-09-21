3a86a8fa677475fb8801fddb0eb86c60a7ccc7bb
zhangxiaomeng <zhangxiaomeng1@jd.com>
2016-09-21 17:52:57 +0800

    init project

---
+++ src/com/zhang/Test.java
@@ -0,0 +1,10 @@
+package com.zhang;
+
+/**
+ * Created by zhangxiaomeng1 on 2016/9/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("开始学习git");
+    }
+}
